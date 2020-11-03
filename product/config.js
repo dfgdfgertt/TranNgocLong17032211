dea8394edcf9c61ebde2dde1c21e989f04b556e0
dfgdfgertt <dfgdfgertt@github.com>
2020-11-03 19:19:33 +0700

    new

---
+++ product/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    aws_table_name: 'tranngoclong',
+    aws_local_config: {
+        region: 'local',
+        endpoint: 'http://18.136.119.168/:3000'
+   
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIA2MU3WQQLKTPDE4MM',
+      secretAccessKey: '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',
+      region: 'ap-southeast-1',
+    }
+}
